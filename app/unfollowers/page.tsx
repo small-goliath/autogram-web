249e6620b9409400d64a81bd1e8e8c9579ca58e0
small-goliath <miy617@gmail.com>
2025-05-29 13:35:03 +0900

    언팔로워 조회 스크립트 실행

--- app/unfollowers/page.tsx
+++ app/unfollowers/page.tsx
@@ -1,5 +1,11 @@
+import Script from 'next/script';
 import { Unfollowers } from "@/components/unfollower";
 
 export default function Page() {
-  return <Unfollowers />;
+  // return <Unfollowers />;
+  return (
+    <>
+      <Script src="/unfollowers.js" strategy="afterInteractive" />
+    </>
+  );
 }
