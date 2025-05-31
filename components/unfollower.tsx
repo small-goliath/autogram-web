d03eb7d678b45142b2c37bf60b784c7760bcfd80
small-goliath <meang617@gmail.com>
2025-05-31 11:18:18 +0900

    feat(unfollower.tsx): PC 및 모바일 버전 비디오 추가 PC와 모바일 버전의 언팔로워 검색기 사용법 비디오를 추가하여 사용자 편의성을 향상시킴.

--- components/unfollower.tsx
+++ components/unfollower.tsx
@@ -3,13 +3,21 @@
 export function Unfollower() {
   return (
     <div className="container">
-      <h1>인스타그램 언팔 검색기</h1>
+      <h1> 🚨 인스타그램 언팔 검색기</h1>
       <div className="form-group">
-        <label> 🚨 언팔 검색 이용 방법</label>
+        <label> 언팔 검색 이용 방법</label>
           <a href="/도토리의_언팔로워_수집기.html" download>북마크 파일 다운로드</a>
+          <hr />
           <div className="verificationCodeVideo">
+            <label> 💻 PC 버전</label>
             <video controls width="100%">
-              <source src="/unfollowers-video.mp4" type="video/mp4" />
+              <source src="/PC-unfollowers-video.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className="verificationCodeVideo">
+            <label> 📱 모바일 버전</label>
+            <video controls width="100%">
+              <source src="/MOBILE-unfollowers-video.mp4" type="video/mp4" />
             </video>
           </div>
       </div>
